d308507656843498882aeb62697f72bc6ec32f4d
Christoffer <chriso-99@hotmail.com>
2021-05-04 10:09:16 +0200

    fix even data generation

---
+++ modules/gen_data_subsets.py
@@ -0,0 +1,23 @@
+from get_data import get_data
+from random import sample
+
+X, y = get_data()
+y = y.where(X.reviewText.str.contains(r'[a-zA-Z]'))
+X = X.where(X.reviewText.str.contains(r'[a-zA-Z]'))
+
+ranges = [10, 50, 100, 500, 2000]
+
+neg = X[y == 0]
+pos = X[y == 1]
+
+for r in ranges:
+    text = []
+    for label, data in enumerate([neg, pos]):
+        idxs = range(len(data))
+        idxs = sample(idxs, r//2)
+        text += list(data.iloc[idxs].reviewText.str.replace('\n', ' '))
+
+    with open(f'../Data/subsets/n_{r}.txt', 'w') as f:
+        for i, t in enumerate(text):
+            l = int(i < r//2)
+            f.write(f'{l}\t{t}\n')
--- modules/get_data.py
+++ modules/get_data.py
@@ -1,31 +1,35 @@
+import numpy as np
+import pandas as pd
+import json
+
 def get_data(type='train'):
 	''' Returns a tuple: (X, target). 
 	This is either train, dev, test or hard data '''
-	paths = {'train':'Data/music_reviews_train.json', \
-			 'dev'  :'Data/music_reviews_dev.json', \
-			 'test' :'Data/music_reviews_test_masked.json' \
-			 'hard' :'Data/phase_2_masked.json' \
+	paths = {'train' : '../Data/music_reviews_train.json', \
+			 'dev'   : '../Data/music_reviews_dev.json', \
+			 'test'  : '../Data/music_reviews_test_masked.json', \
+			 'hard'  : '../Data/phase_2_masked.json', \
 			 }
 	path = paths[type]
 	
-    data = []
-    cols = {'verified':0,'reviewTime':1,'reviewerID':2,'asin':3,"reviewText":4,"summary":5,"unixReviewTime":6,"sentiment":7,"id":8}
-    for line in open(path):
-        review_data = json.loads(line)
+	data = []
+	cols = {'verified':0,'reviewTime':1,'reviewerID':2,'asin':3,"reviewText":4,"summary":5,"unixReviewTime":6,"sentiment":7,"id":8}
+	for line in open(path):
+		review_data = json.loads(line)
 		
-        row = [None]*len(cols)
-        for key in review_data:
-            if key in cols:
-                if key == "sentiment":
-                    row[cols[key]] = 1 if review_data[key] == "positive" else 0
-                else:
-                    row[cols[key]] = str(review_data[key])
-        data.append(row)
-    X = pd.DataFrame(data, columns=cols)
+		row = [None]*len(cols)
+		for key in review_data:
+			if key in cols:
+				if key == "sentiment":
+					row[cols[key]] = 1 if review_data[key] == "positive" else 0
+				else:
+					row[cols[key]] = str(review_data[key])
+		data.append(row)
+	X = pd.DataFrame(data, columns=cols)
 	
-    # set empty reviews to '' (instead of None)
-    X.loc[X['reviewText'].isna(), 'reviewText'] = ''
-    X.loc[X['summary'].isna(), 'summary'] = ''
-    y = X['sentiment']
-    X.drop(columns='sentiment', inplace=True)
-    return X, y
+	# set empty reviews to '' (instead of None)
+	X.loc[X['reviewText'].isna(), 'reviewText'] = ''
+	X.loc[X['summary'].isna(), 'summary'] = ''
+	y = X['sentiment']
+	X.drop(columns='sentiment', inplace=True)
+	return X, y
