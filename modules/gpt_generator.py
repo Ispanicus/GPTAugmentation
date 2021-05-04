8a4fbe48321a7c9012ce20f7219b49bc176194c8
Ispanicus <alai@itu.dk>
2021-05-04 10:41:58 +0200

    updated gpt generator

--- modules/gpt_generator.py
+++ modules/gpt_generator.py
@@ -1,5 +1,22 @@
-with open("../Data/subsets/n_10.txt") as f:
-    outfile = open("../Data/subsets/gpt_10.txt", "w")
-    for line in f.readlines():
-        t = line.strip().split("\t")
-        outfile.write(f'Amazon review: "{t[1]}"\nSentiment: Positive\n###\n')
+from random import sample
+
+ranges = [10, 50, 100, 500, 2000]
+dic = {"0":"Negative", "1":"Positive"}
+
+
+for i in ranges:
+    with open(f"../Data/subsets/n_{i}.txt") as file:
+        cases = file.readlines()
+    for sentiment in ["Negative", "Positive"]:
+        outfile = open(f"../Data/subsets/gpt_{sentiment}_{i}.txt", "w")
+        
+        if sentiment == "Positive":
+            idx = sample(range(0,i//2), 4)
+        else:
+            idx = sample(range(i//2,i), 4)
+            
+        for j in idx:
+            _, text = cases[j].strip().split("\t")
+            outfile.write(f'Amazon review: "{text}"\nSentiment: {sentiment}\n###\n')
+        outfile.write('Amazon review: ')
+        outfile.close()
