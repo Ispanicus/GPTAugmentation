d776e95d64b16517dac0186841e35773c03b7ca1
Haervaerk <45241565+Haervaerk@users.noreply.github.com>
2021-05-26 15:26:10 +0200

    Clean_text function added

--- modules/cleaner.py
+++ modules/cleaner.py
@@ -14,6 +14,7 @@ contractions = [("aren't", "are not"),
 ("he'd", "he would"),
 ("he'll", "he will"),
 ("he's", "he is"),
+("here's", "here is"),
 ("i'd", "i would"),
 ("i'll", "i will"),
 ("it's", "it is"),
@@ -65,6 +66,8 @@ def compare_clean_vocab(X):
                 label, text = l.split('\t')
                 vocab.update(word_tokenize(text))
     print(f"{path}\ncleaned {len(base_vocab):>6} words, removed {len(base_vocab - clean_vocab)}")
+    
+    
 
 def clean(path):
     lemmatize = WordNetLemmatizer().lemmatize
@@ -81,6 +84,23 @@ def clean(path):
                 text = text.replace("'", '') #remove ' and "
                 tokens = [lemmatize(w) for w in word_tokenize(text)]
                 outfile.write(f'{label}\t{" ".join(tokens)}\n')
+
+
+def clean_text(text):
+    lemmatizer = WordNetLemmatizer().lemmatize
+    text = text.lower()
+    pattern1 = re.compile(r'([^0-9a-zA-Z\s])\1+(?=[a-z0-9A-Z])')
+    pattern2 = re.compile(r'([^0-9a-zA-Z\s])\1+')
+    pattern3 = re.compile(r'<[^>]>')
+    text = re.sub(pattern1, r'\1 ', text)
+    text = re.sub(pattern2, r'\1', text)
+    text = re.sub(pattern3, r'', text)
+    for contr, exp in contractions:
+        text = text.replace(contr, exp)
+    text = text.replace('"', "'")
+    text = text.replace("'", '') #remove ' and "
+    tokens = " ".join([lemmatizer(w) for w in word_tokenize(text)])
+    return tokens
 
 
 def compare_clean_vocab(path):
